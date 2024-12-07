4dd6e7487a20bbb3cc77d8b953845af5d9bca5c3
Ömer Çevrük <omer.cevrukk@gmail.com>
2024-12-07 20:06:32 +0300

    completes the part2
    apperently there are two parts to each day now Im doing them

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections;
 use std::collections::VecDeque;
 use std::fs;
-fn main() {
+fn part1() {
     let content = fs::read_to_string("input.txt").expect("Could not read the file");
     let (mut left, mut right): (Vec<i32>, Vec<i32>) = content
         .lines()
@@ -29,4 +29,30 @@ fn main() {
     }
 
     println!("{:?}", distance);
+}
+fn part2() {
+    let mut total = 0;
+    let content = fs::read_to_string("input.txt").expect("Could not read the file");
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = content
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| {
+            let mut nums = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().expect("Not a valid number"));
+            (
+                nums.next().expect("Missing first number"),
+                nums.next().expect("Missing second number"),
+            )
+        })
+        .unzip();
+    for i in left {
+        let count = right.iter().filter(|num| **num == i).count();
+        total += count as i32 * i;
+    }
+    println!("{}", total);
+}
+fn main() {
+    // part1();
+    part2();
 }
